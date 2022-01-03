87023d850a5375143a7f40de2dc55d09f4195e8e
Chathura Harshanga <38973288+chathuraHarshanga@users.noreply.github.com>
2022-01-03 20:13:47 +0530

    add users method

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,31 @@ import express from "express";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('hello from users');
+const users = [
+  {
+    name: "Jone",
+    LastName: "Doe",
+    age: 25,
+  },
+  {
+    name: "Zoe",
+    LastName: "Maddison",
+    age: 22,
+  },
+];
+
+//view all users
+router.get("/", (req, res) => {
+  res.send(users);
+});
+
+//create users
+router.post("/", (req, res) => {
+  const user = req.body;
+
+  users.push(user);
+
+  res.send(`${user.name} added to the database`);
 });
 
 export default router;
